bc694649128d061682f9dbd9e6848616566f21c3
권라임 <punchrime@seoultech.ac.kr>
2022-07-12 23:37:45 +0900

    [{라임}2주차 과제_{2577_숫자의 개수}]

---
+++ ACDCBoostCourse/ACDCBoostCourse/Algortihm/Week 2/week2_hw2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	int c;
+
+	int arr[10] = { 0 };
+	cin >> a >> b >> c;
+	int result = a * b * c;
+
+	while (result != 0) {
+		arr[result % 10] += 1;
+		result /= 10;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << endl;
+	}
+
+	return 0;
+
+
+
+}
